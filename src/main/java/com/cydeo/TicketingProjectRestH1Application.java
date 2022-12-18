db1bd806645b812c46b6f6f289b9dc1e936aeca3
hiraferda <hirayavuz23ferda@gmail.com>
2022-12-18 16:49:20 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/TicketingProjectRestH1Application.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectRestH1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectRestH1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/ticketingprojectresth1/TicketingProjectRestH1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingprojectresth1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectRestH1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
